68640c72ca7ab70b28b402f24f00063e54ca2391
Sweodin <petergustafsson910@gmail.com>
2024-10-15 08:28:13 +0200

    first commit added index.html and app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+// active state to use console.log
+
+// console.log("Hello world!"); 
+
+// active state to use stored information
+
+ // let x = "Peter Gustafsson";
+ // let z = "39 years old";
+ // let y = "Black";
+
+ // console.log(x + " is " + z + " years old and his favorite color is " + y);
+
+ // active state to do math with console.log
+
+// let a = 1;
+
+// let b = 4;
+
+// let c = 7;
+
+// console.log();
